6eb8f3432e7c4ca4550fabc5cadd3ffad9525435
Yoshimura Yuu <m@yyu.pw>
2016-08-27 19:52:56 +0900

    first commit

---
+++ install-tex.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+  curl -L -O http://mirrors.concertpass.com/tex-archive/systems/mac/mactex/BasicTeX.pkg
+  sudo installer -pkg BasicTeX.pkg -target /
+  rm BasicTeX.pkg
+  export PATH=$PATH:/usr/texbin
+else
+  sudo add-apt-repository -y ppa:jonathonf/texlive-2016
+  sudo apt-get update -qq
+  sudo apt-get install -y texlive
+fi
