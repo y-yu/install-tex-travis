633ef75d26ef82e93d444c3f69fc362e7e3c4a70
Yoshimura Yuu <m@yyu.pw>
2016-08-29 01:23:57 +0900

    fix raw file urls

--- install-tex.sh
+++ install-tex.sh
@@ -12,8 +12,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   rm BasicTeX.pkg
   export PATH=$PATH:/usr/texbin
 else
-  wget https://github.com/scottkosty/install-tl-ubuntu/raw/master/install-tl-ubuntu && chmod +x ./install-tl-ubuntu
-  wget https://github.com/y-yu/install-tex-travis/raw/master/small.profile
+  wget https://raw.githubusercontent.com/scottkosty/install-tl-ubuntu/master/install-tl-ubuntu && chmod +x ./install-tl-ubuntu
+  wget https://raw.githubusercontent.com/y-yu/install-tex-travis/master/small.profile
   sudo ./install-tl-ubuntu -p `pwd`/small.profile -q http://ctan.mirror.rafal.ca/systems/texlive/tlnet/
   export PATH=$PATH:/opt/texbin
   tlmgr init-usertree
