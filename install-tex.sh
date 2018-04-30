b7682b3a2064db9b4bdafaac219a22fbd776b9ed
Yoshimura Yuu <yyu@mental.poker>
2018-04-30 17:00:29 +0900

    Fix install path of TeXLive

--- install-tex.sh
+++ install-tex.sh
@@ -15,13 +15,13 @@ cd install-tl-*
 
 BASE_PROFILE=$(cat << EOS
 selected_scheme scheme-small
-TEXDIR $HOME/texlive/2018
-TEXMFCONFIG $HOME/.texlive2018/texmf-config
+TEXDIR $HOME/texlive
+TEXMFCONFIG $HOME/.texlive/texmf-config
 TEXMFHOME $HOME/texmf
 TEXMFLOCAL $HOME/texlive/texmf-local
-TEXMFSYSCONFIG $HOME/texlive/2018/texmf-config
-TEXMFSYSVAR $HOME/texlive/2018/texmf-var
-TEXMFVAR $HOME/.texlive2018/texmf-var
+TEXMFSYSCONFIG $HOME/texlive/texmf-config
+TEXMFSYSVAR $HOME/texlive/texmf-var
+TEXMFVAR $HOME/.texlive/texmf-var
 option_doc 0
 option_src 0
 EOS
@@ -29,10 +29,10 @@ EOS
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   echo "$BASE_PROFILE\nbinary_x86_64-darwin 1" > ./small.profile
-  export PATH=$PATH:$HOME/texlive/2018/bin/x86_64-darwin
+  export PATH=$PATH:$HOME/texlive/bin/x86_64-darwin
 else
   echo "$BASE_PROFILE\nbinary_x86_64-linux 1" > ./small.profile
-  export PATH=$PATH:$HOME/texlive/2018/bin/x86_64-linux
+  export PATH=$PATH:$HOME/texlive/bin/x86_64-linux
 fi
 
 ./install-tl -profile ./small.profile -repository http://ctan.mirror.rafal.ca/systems/texlive/tlnet
